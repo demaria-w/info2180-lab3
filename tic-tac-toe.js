89a2da15801d569c3d0d2b6804017e397c381260
Demaria Walker <demaria.walker456@gmail.com>
2025-10-05 13:54:24 -0500

    Adding an X or O when square is clicked and ensures user can't change squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,5 +8,29 @@ document.addEventListener('DOMContentLoaded', () => { //for when the page loads
     boardSquares.forEach(square =>{
         square.classList.add('square');
     });
+
+    
+//Exercise 2 - Add an X or O to a square when clicked   
+
+    let currentPlayer = 'X';
+
+    game = Array(9).fill(null); //keeps track of the game
+
+    boardSquares.forEach((square,index) => {
+        square.addEventListener('click',() => {
+            if (!square.textContent) { //prevents the box from being overwritten
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+                game[index] = currentPlayer;
+
+                //switches between layers
+                if(currentPlayer === 'X'){
+                    currentPlayer = 'O';
+                }else {
+                    currentPlayer = 'X';
+                }
+            }
+        })
+    })
 });
 
